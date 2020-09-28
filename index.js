0f5d350db0861cb62ac843692c074d32a207ec87
cauxNicolas <nicaux95@gmail.com>
2020-09-28 23:10:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+app.listen(3100, () => {
+  console.log("server CRUD started");
+});
