c247fd24a20497772f25ab98cb00debbb26e855d
cauxNicolas <nicaux95@gmail.com>
2020-10-31 14:26:40 +0100

    login OK

--- routes/Login.js
+++ routes/Login.js
@@ -8,16 +8,25 @@ const SHA256 = require("crypto-js/sha256");
 
 router.post("/", async (req, res) => {
   try {
-    console.log(req.fields);
     const email = req.fields.email;
     const password = req.fields.password;
 
-    const search = await User.findOne({ email: email });
-    console.log("search ->", search);
-    if (search) {
-      const newHash = SHA256(password + search.salt).toString(enBase64);
-      if (newHash === search.hash) {
-        res.status(200).json("page login ok");
+    if (req.fields.email === "" || req.fields.password === "") {
+      res.status(400).json("remplir les champs");
+    } else if (
+      !email.replace(/\s+/, "").length ||
+      !password.replace(/\s+/, "").length
+    ) {
+      res.status(400).json("pas de champs vides");
+    } else {
+      const search = await User.findOne({ email: email });
+      if (search) {
+        const newHash = SHA256(password + search.salt).toString(enBase64);
+        if (newHash === search.hash) {
+          res.status(200).json("page login ok");
+        } else {
+          res.status(400).json("Email ou mot de passe erronés");
+        }
       } else {
         res.status(400).json("Email ou mot de passe erronés");
       }
